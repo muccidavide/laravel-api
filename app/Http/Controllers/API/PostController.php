426ae215e446b0e5ea524a57c98b368fab9c16f3
muccidavide <davidemucci0804@gmail.com>
2022-07-06 13:02:50 +0200

    initial commit - complete api call

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with(['tags', 'category'])->orderByDesc('id')->paginate(9);
+        return $posts;
+    }
+
+    public function show($slug)
+    {
+        
+        $post = Post::with(['tags', 'category'])->where('slug', $slug)->first();
+        
+        if ($post) {
+            return $post;
+        } else {
+            return response()->json([
+                'status' => 404,
+                'status_text' => 'not found',
+            ]);
+            
+        }
+        
+    }
+}
